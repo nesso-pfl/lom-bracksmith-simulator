7366b1c0c50e290983af011e2c469d2956e47316
nesso <32949286+nesso-pfl@users.noreply.github.com>
2023-07-02 22:31:38 +0900

    Setup prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,6 +4,8 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
