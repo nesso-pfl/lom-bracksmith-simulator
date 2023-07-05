2617d163bd5de30f5b36461c8a6693e3700e7f7b
nesso <32949286+nesso-pfl@users.noreply.github.com>
2023-07-05 12:48:28 +0900

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,35 @@ import {
 } from "./components"
 import { baseMaterialData, weaponData } from "./data"
 import { useState } from "react"
-import { SideMaterial } from "./logic"
+import { SideMaterial, Weapon } from "./logic"
 
 function App() {
+  const [weapon, setWeapon] = useState<Weapon>({
+    material: baseMaterialData[0],
+    type: weaponData[0],
+    bigTricks: [],
+    status: {
+      power: 0,
+      technique: 0,
+      defence: 0,
+      magic: 0,
+      physical: 0,
+      spirit: 0,
+      charm: 0,
+      luck: 0,
+    },
+    element: {
+      light: 0,
+      dark: 0,
+      wood: 0,
+      metal: 0,
+      fire: 0,
+      earth: 0,
+      wind: 0,
+      water: 0,
+    },
+    secretPowers: [],
+  })
   const [selectedSideMaterials, setSelectedSideMaterials] = useState<SideMaterial[]>([])
 
   return (
@@ -20,37 +46,14 @@ function App() {
         </Heading>
         <Flex mb={5} gap={8}>
           <BaseMaterialAndWeaponTypeForm
-            onChangeBaseMaterial={(e) => console.log(e)}
-            onChangeWeaponType={(e) => console.log(e)}
-          />
-          <WeaponStatus
-            weapon={{
-              material: baseMaterialData[0],
-              type: weaponData[0],
-              bigTricks: [],
-              status: {
-                power: 2,
-                technique: 2,
-                defence: 2,
-                magic: 2,
-                physical: 2,
-                spirit: 2,
-                charm: 2,
-                luck: 2,
-              },
-              element: {
-                light: 2,
-                dark: 2,
-                wood: 2,
-                metal: 2,
-                fire: 2,
-                earth: 2,
-                wind: 2,
-                water: 2,
-              },
-              secretPowers: [],
+            onChangeBaseMaterial={(material) => {
+              setWeapon((current) => ({ ...current, material }))
+            }}
+            onChangeWeaponType={(weaponType) => {
+              setWeapon((current) => ({ ...current, type: weaponType }))
             }}
           />
+          <WeaponStatus weapon={weapon} />
         </Flex>
         <Flex>
           <Box>
