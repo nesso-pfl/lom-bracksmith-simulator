1c2a47735a54372c1a365b8f1747bcb0fd3fb7a6
nesso <32949286+nesso-pfl@users.noreply.github.com>
2023-08-19 01:29:26 +0900

    Impl canForge

--- src/logic/blackSmith.ts
+++ src/logic/blackSmith.ts
@@ -108,9 +108,42 @@ const calcRequiredEnergy = (blackSmith: BlackSmith, element: Element) => {
     : resistance * Math.pow(2, level)
 }
 
+const canForge = (blackSmith: BlackSmith, element: Element): boolean => {
+  const {
+    extractedSecretPower,
+    weapon: { reservedSecretPower, secretPowers, element: weaponElement },
+  } = blackSmith
+  const hasAncientMoon = [reservedSecretPower, ...secretPowers, extractedSecretPower].find((sp) => sp === "太古の月")
+  if (hasAncientMoon) true
+
+  const hasMirrorWorld = [reservedSecretPower, ...secretPowers, extractedSecretPower].find((sp) => sp === "鏡面世界")
+  switch (element) {
+    case "light":
+      return !(hasMirrorWorld && weaponElement.dark > 0)
+    case "dark":
+      return !!hasMirrorWorld || weaponElement.light === 0
+    case "wood":
+      return hasMirrorWorld
+        ? weaponElement.dark >= weaponElement.light || weaponElement.metal === 0
+        : weaponElement.light >= weaponElement.dark || weaponElement.metal === 0
+    case "metal":
+      return hasMirrorWorld
+        ? weaponElement.light >= weaponElement.dark || weaponElement.wood === 0
+        : weaponElement.dark >= weaponElement.light || weaponElement.wood === 0
+    case "fire":
+      return hasMirrorWorld ? weaponElement.earth === 0 : weaponElement.water === 0
+    case "earth":
+      return hasMirrorWorld ? weaponElement.wind === 0 : weaponElement.fire === 0
+    case "wind":
+      return hasMirrorWorld ? weaponElement.water === 0 : weaponElement.earth === 0
+    case "water":
+      return hasMirrorWorld ? weaponElement.fire === 0 : weaponElement.wind === 0
+  }
+}
+
 const forge = (blackSmith: BlackSmith, element: Element) => {
   const requiredEnergy = calcRequiredEnergy(blackSmith, element)
-  return blackSmith.currentEnergy >= requiredEnergy
+  return canForge(blackSmith, element) && blackSmith.currentEnergy >= requiredEnergy
     ? {
         ...blackSmith,
         weapon: {
